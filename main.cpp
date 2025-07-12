ff08af4ac7566c59d44500b64e0f5fe051ac7baf
Beta-Verse-Hub <baibhavkumarjha1@gmail.com>
2025-07-12 15:31:18 +0530

    Read the code and print it into the console

--- main.cpp
+++ main.cpp
@@ -1,9 +1,35 @@
+#include <fstream>
 #include <iostream>
+#include <string>
+#include <vector>
 
 using namespace std;
+using vs = vector<string>;
+
+int C_OUTPUT(){
+    
+    return 0;
+}
+
+int readCode(){
+    int status = 0;
+
+    ifstream inputProgramFile("MyProgram.vri");
+
+    string line;
+    vs inputProgram;
+    
+    while (getline(inputProgramFile, line)) {
+        inputProgram.push_back(line);
+    };
+
+    for(int lineNumber = 0; lineNumber < inputProgram.size(); lineNumber++){
+        cout << inputProgram.at(lineNumber) << endl;
+    }
+
+    return status;
+}
 
 int main(){
-    cout << "Hello World!" << endl;
-
-    return 0;
+    return readCode();
 }
